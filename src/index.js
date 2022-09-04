5709d83c39608c22ec515b2aaf1c878b63104ca8
Loïc GRENON <loic.grenon@difoolou.net>
2022-09-04 15:37:41 +0200

    Register a route via Settings -> ExecOnMcode instead of Machine-Specific tab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 'use strict'
 
-import { registerSettingTab } from '../../routes'
-
+import { registerRoute } from '../../routes'
 import ExecOnMcode from './ExecOnMcode.vue'
-// Register a setting tab on the Machine-specific settings page
-registerSettingTab(false, 'settings-machine-ExecOnMcode', ExecOnMcode, 'ExecOnMcode');
+// Register a route via Settings -> ExecOnMcode
+registerRoute(ExecOnMcode, {
+	Settings: {
+		ExecOnMcode: {
+			icon: 'mdi-powershell',
+			caption: 'ExecOnMcode',
+			path: '/ExecOnMcode'
+		}
+	}
+});
